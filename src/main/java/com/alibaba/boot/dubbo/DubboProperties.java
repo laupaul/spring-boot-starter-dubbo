75e494c7b793842c5e8ea080031de2e1320d77f6
liupeng <liupeng@172.16.50.26>
2017-12-05 16:02:11 +0800

    add auto annotation

--- src/main/java/com/alibaba/boot/dubbo/DubboProperties.java
+++ src/main/java/com/alibaba/boot/dubbo/DubboProperties.java
@@ -11,97 +11,113 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "spring.dubbo")
 public class DubboProperties {
-  /**
-   * dubbo application name
-   */
-  private String appname;
-  /**
-   * dubbo registry address
-   */
-  private String registry;
-  /**
-   * communication protocol, default is dubbo
-   */
-  private String protocol = "dubbo";
-  /**
-   * dubbo listen port, default 20800
-   */
-  private int port = 20800;
-  /**
-   * dubbo thread count, default 200
-   */
-  private int threads = 200;
+	/**
+	 * dubbo application name
+	 */
+	private String appname;
+	/**
+	 * dubbo registry address
+	 */
+	private String registry;
+	/**
+	 * communication protocol, default is dubbo
+	 */
+	private String protocol = "dubbo";
+	/**
+	 * dubbo listen port, default 20800
+	 */
+	private int port = 20800;
+	/**
+	 * dubbo thread count, default 200
+	 */
+	private int threads = 200;
 
-  /**
-   * dubbo version, may override by {@link com.alibaba.dubbo.config.annotation.Service#version()}
-   */
-  private String version = "";
+	/**
+	 * dubbo version, may override by
+	 * {@link com.alibaba.dubbo.config.annotation.Service#version()}
+	 */
+	private String version = "";
 
-  /**
-   * dubbo group, may override by {@link com.alibaba.dubbo.config.annotation.Service#group()}
-   */
-  private String group = "";
+	/**
+	 * dubbo group, may override by
+	 * {@link com.alibaba.dubbo.config.annotation.Service#group()}
+	 */
+	private String group = "";
 
-  public String getAppname() {
-    return this.appname;
-  }
+	/**
+	 * package base dir 根据包路径扫描，自动注册
+	 * 
+	 * @author liupeng
+	 */
+	private String scan;
 
-  public void setAppname(String appname) {
-    this.appname = appname;
-  }
+	public String getAppname() {
+		return this.appname;
+	}
 
-  public String getRegistry() {
-    return this.registry;
-  }
+	public void setAppname(String appname) {
+		this.appname = appname;
+	}
 
-  public void setRegistry(String registry) {
-    this.registry = registry;
-  }
+	public String getRegistry() {
+		return this.registry;
+	}
 
-  public String getProtocol() {
-    return this.protocol;
-  }
+	public void setRegistry(String registry) {
+		this.registry = registry;
+	}
 
-  public void setProtocol(String protocol) {
-    this.protocol = protocol;
-  }
+	public String getProtocol() {
+		return this.protocol;
+	}
 
-  public int getPort() {
-    return this.port;
-  }
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
 
-  public void setPort(int port) {
-    this.port = port;
-  }
+	public int getPort() {
+		return this.port;
+	}
 
-  public int getThreads() {
-    return this.threads;
-  }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-  public void setThreads(int threads) {
-    this.threads = threads;
-  }
+	public int getThreads() {
+		return this.threads;
+	}
 
-  public String getVersion() {
-    return this.version;
-  }
+	public void setThreads(int threads) {
+		this.threads = threads;
+	}
 
-  public void setVersion(String version) {
-    this.version = version;
-  }
+	public String getVersion() {
+		return this.version;
+	}
 
-  public String getGroup() {
-    return this.group;
-  }
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
-  public void setGroup(String group) {
-    this.group = group;
-  }
+	public String getGroup() {
+		return this.group;
+	}
 
-  @Override
-  public String toString() {
-    return "DubboProperties [appname=" + this.appname + ", registry=" + this.registry
-        + ", protocol=" + this.protocol + ", port=" + this.port + ", threads=" + this.threads
-        + ", version=" + this.version + ", group=" + this.group + "]";
-  }
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getScan() {
+		return scan;
+	}
+
+	public void setScan(String scan) {
+		this.scan = scan;
+	}
+
+	@Override
+	public String toString() {
+		return "DubboProperties [appname=" + this.appname + ", registry=" + this.registry + ", protocol=" + this.protocol + ", port=" + this.port + ", threads=" + this.threads
+				+ ", version=" + this.version + ", group=" + this.group + "]";
+	}
 }
